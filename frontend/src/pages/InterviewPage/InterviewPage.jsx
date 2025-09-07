d269cb44f9f2a3caff1f08067a8af67bd8c24583
msaghiri <mahmoodalsaghiri@gmail.com>
2025-09-07 11:19:46 -0400

    Fixed bugs with code editor

--- frontend/src/pages/InterviewPage/InterviewPage.jsx
+++ frontend/src/pages/InterviewPage/InterviewPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import ConversationComponent from "../../components/ConversationComponent/ConversationComponent.jsx";
 import Editor from "react-simple-code-editor";
@@ -10,30 +10,32 @@ import style from "./InterviewPage.module.css";
 function InterviewPage() {
 	const [code, setCode] = useState("");
 
+	const highlightCode = useCallback(
+		(code) => Prism.highlight(code, Prism.languages.python, "python"),
+		[]
+	);
+
 	return (
 		<div className={style.interviewPageContainer}>
-			<div className={style.contentBorder}>
-				<div className={style.editorBackground}>
-					<Editor
-						value={code}
-						onValueChange={(code) => setCode(code)}
-						highlight={(code) =>
-							Prism.highlight(code, Prism.languages.python, "python")
-						}
-						padding={20}
-						style={{
-							fontFamily: '"Fira code", "Fira Mono", monospace',
-							fontSize: 14,
-							width: "100%",
-							height: "100%",
-							background: "transparent",
-							borderRadius: "40px",
-						}}
-						textareaClassName={style.codeEditorTextArea}
-					/>
-				</div>
-				<ConversationComponent className={style.conversationComponent} />
+			<div className={style.editorBackground}>
+				<Editor
+					value={code}
+					onValueChange={(code) => setCode(code)}
+					highlight={highlightCode}
+					style={{
+						fontFamily: '"Fira code", "Fira Mono", monospace',
+						fontSize: 14,
+						width: "100%",
+						minHeight: "100%",
+						whiteSpace: "pre",
+						background: "transparent",
+					}}
+					padding={30}
+					tabSize={4}
+					textareaClassName={style.codeEditorTextArea}
+				/>
 			</div>
+			<ConversationComponent className={style.conversationComponent} />
 		</div>
 	);
 }
