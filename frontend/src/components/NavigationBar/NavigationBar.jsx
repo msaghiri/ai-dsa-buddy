2ee6fac69af8193bd1fa848d809da5b1c958c9c3
msaghiri <mahmoodalsaghiri@gmail.com>
2025-09-07 00:59:26 -0400

    navbar ui

--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -4,15 +4,21 @@ import style from "./NavigationBar.module.css";
 function NavigationBar() {
 	return (
 		<nav className={style.navigationBar}>
+			<p className={style.logo}>LOGO</p>
 			<ul className={style.navigationList}>
 				<li className={style.navigationItem}>
-					<Link className={style.noUnderline} to="/login">
-						Login
+					<Link className={style.navigationItemText} to="/login">
+						Sign in
 					</Link>
 				</li>
 				<li className={style.navigationItem}>
-					<Link className={style.noUnderline} to="/conversation">
-						Welcome
+					<Link className={style.navigationItemText} to="/conversation">
+						Chat
+					</Link>
+				</li>
+				<li className={style.navigationItem}>
+					<Link className={style.navigationItemText} to="/">
+						Placeholder
 					</Link>
 				</li>
 			</ul>
