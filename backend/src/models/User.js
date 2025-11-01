645098df83bb7fe6e13d1850d904b01a74c012f5
msaghiri <mahmoodalsaghiri@gmail.com>
2025-11-01 19:16:30 -0400

    database fixes -- migrating to local database for dev

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -6,4 +6,4 @@ const UserSchema = new mongoose.Schema({
 	email: String,
 });
 
-export const User = mongoose.model("User", UserSchema);
+export const User = mongoose.model("User", UserSchema, "users");
