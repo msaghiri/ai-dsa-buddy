683d5b2cb0b013cfa37f2fdc17c5d0a456887046
msaghiri <mahmoodalsaghiri@gmail.com>
2025-11-08 13:16:37 -0500

    first iteration of interview feedback capabilities in the backend

--- backend/src/interviews/interviewManager.js
+++ backend/src/interviews/interviewManager.js
@@ -1,4 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
+import { Type } from "@google/genai";
 
 import config from "../config.js";
 import questions from "../questions/questions.js";
@@ -118,8 +119,71 @@ export async function sendMessage(userId, message) {
 	return response.text;
 }
 
+export async function getInterviewFeedback(userId) {
+	const session = interviewSessions[userId];
+	const questionPrompt = session.question.prompt;
+	const chatHistory = session.chat.history;
+
+	const informationObject = {
+		question: questionPrompt,
+		history: chatHistory,
+	};
+
+	const prompt = `
+You are an expert Senior Software Engineer and a highly-skilled technical interviewer.
+Your task is to provide a critical evaluation of a candidate's mock interview performance based on the provided data.
+
+Analyze the 'question' and 'history' from the JSON data below to assess the candidate's reasoning and communication skills.
+
+You must provide your evaluation by filling out all fields of the requested JSON schema.
+
+---
+INTERVIEW DATA:
+${JSON.stringify(informationObject)}
+---
+`;
+
+	const feedbackSchema = {
+		type: Type.OBJECT,
+		properties: {
+			summary: {
+				type: Type.STRING,
+				description:
+					"A concise, two or three sentence long paragraph summarizing the candidates performance. Make sure to concisely outline the candidates strengths and weaknesses in programming ability, reasoning, and ability to explain and communicate their thoughts.",
+			},
+			reasoningRating: {
+				type: Type.NUMBER,
+				description:
+					"A numerical score from 1 (Poor) to 10 (Excellent) of the candidates reasoning, pattern recognition, and/or logic demonstrated in their approach to the problem.",
+				minimum: 1,
+				maximum: 10,
+			},
+			communicationRating: {
+				type: Type.NUMBER,
+				description:
+					"A numerical score from 1 (Poor) to 10 (Excellent) of the candidates communication abilities based on the interview transcript.",
+				minimum: 1,
+				maximum: 10,
+			},
+		},
+	};
+
+	const response = await ai.models.generateContent({
+		model: config.GEMINI_MODEL,
+		contents: prompt,
+		config: {
+			responseMimeType: "application/json",
+			responseSchema: feedbackSchema,
+		},
+	});
+
+	console.log(response.text);
+}
+
 export function destroyInterviewSession(userId) {
 	if (!interviewSessionExists(userId)) return false;
+
+	getInterviewFeedback(userId);
 
 	delete interviewSessions[userId];
 
